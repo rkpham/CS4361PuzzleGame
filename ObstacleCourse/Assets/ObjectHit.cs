1eb9474369e13a7b40a741b6819183ba058cc53b
dibarra3 <160536459+dibarra3@users.noreply.github.com>
2024-11-14 22:12:33 -0600

    Add files via upload

---
+++ ObstacleCourse/Assets/ObjectHit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision other) 
+   {
+        //Debug.Log("Bumped into wall");
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
+   }
+}
