9ef6572b725f771b6998370baca3e8f78e958838
rkpham <ryankpham@gmail.com>
2024-11-21 02:04:37 -0600

    Main menu effects

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private float color = 0.0f;
+
+    [SerializeField] private Image BackgroundImage;
+
+    private void FixedUpdate()
+    {
+        BackgroundImage.color = Color.HSVToRGB(color, 0.4f, 0.6f);
+        color += Time.fixedDeltaTime * 0.1f;
+        if (color > 1.0f)
+            color = 0.0f;
+    }
+
     public void OnPlayPressed()
     {
         LevelManager.LoadNextLevel();
